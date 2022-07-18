afa5dfc98a940e90815f06b9118de8b0e097a221
Jonathan Cain <jcainuk@gmail.com>
2022-07-18 15:56:17 +0100

    add the basic routes for the practise project

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('profiles');
+});
+
+router.get('/new-user', (req, res) => {
+  res.render('new-user');
+});
+
+module.exports = router;
