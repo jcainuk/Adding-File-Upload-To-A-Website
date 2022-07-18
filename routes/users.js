fe7aa1bd198c9a030a9d19fdd49aac5ec90e62b7
Jonathan Cain <jcainuk@gmail.com>
2022-07-18 17:07:47 +0100

    configured multer to use my own unique file name that does still have the correct extension

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const multer = require('multer');
 
-const upload = multer({ dest: 'images' });
+const storageConfig = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'images');
+  },
+  filename(req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
 router.get('/', (req, res) => {
