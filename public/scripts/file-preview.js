14e14bdf1310bfcf673d9078530b1e63428b8135
Jonathan Cain <jcainuk@gmail.com>
2022-07-19 15:04:54 +0100

    set the imagePreviewElement style display properties to toggle between none and block depending on whether a local file is present

--- public/scripts/file-preview.js
+++ public/scripts/file-preview.js
@@ -4,12 +4,14 @@ const imagePreviewElement = document.getElementById('image-preview');
 const showPreview = () => {
   const { files } = filePickerElement;
   if (!files || files.length === 0) {
+    imagePreviewElement.style.display = 'none';
     return;
   }
   const pickedFile = files[0];
 
   // Create a local URL that only works on user's computer
   imagePreviewElement.src = URL.createObjectURL(pickedFile);
+  imagePreviewElement.style.display = 'block';
 };
 
 filePickerElement.addEventListener('change', showPreview);
