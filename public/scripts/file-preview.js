1f41ab269b0b24ee5ff0f9466fe9e3e8c92b3667
Jonathan Cain <jcainuk@gmail.com>
2022-07-19 14:59:42 +0100

    add change event listener and start showPreview function

--- public/scripts/file-preview.js
+++ public/scripts/file-preview.js
@@ -1,2 +1,12 @@
 const filePickerElement = document.getElementById('image');
 const imagePreviewElement = document.getElementById('image-preview');
+
+const showPreview = () => {
+  const { files } = filePickerElement;
+  if (!files || files.length === 0) {
+    return;
+  }
+  const pickedFile = files[0];
+};
+
+filePickerElement.addEventListener('change', showPreview);
